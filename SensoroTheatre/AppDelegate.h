32f363296d55d53347fe272fbeb8fc368981195d
Lucia Rodinò <lucia@macbook-pro-di-lucia.local>
2018-04-27 16:52:23 +0200

    Initial Commit

---
+++ SensoroTheatre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SensoroTheatre
+//
+//  Created by Lucia Rodinò on 27/04/18.
+//  Copyright © 2018 Lucia Rodinò. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SensoroTheatre/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SensoroTheatre
+//
+//  Created by Lucia Rodinò on 27/04/18.
+//  Copyright © 2018 Lucia Rodinò. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
